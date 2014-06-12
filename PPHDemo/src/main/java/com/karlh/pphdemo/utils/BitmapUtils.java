4d81744a7e864211b2ac7c00114fdfb50e992bdd
Karljr79 <karljr79@gmail.com>
2014-06-12 10:08:59 -0700

    Added PPH SDK

---
+++ PPHDemo/src/main/java/com/karlh/pphdemo/utils/BitmapUtils.java
@@ -0,0 +1,51 @@
+/**
+ * Created by khirschhornjr on 6/12/14.
+ */
+
+package com.karlh.pphdemo.utils;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import java.io.ByteArrayOutputStream;
+
+
+/**
+ * This utility class is meant to peform certain bitmap manipulation operations.
+ */
+public class BitmapUtils {
+
+    private static volatile Bitmap bitmap;
+
+    /**
+     * This method converts a bitmap image to a byte array.
+     *
+     * @param bm
+     * @return
+     */
+    public static byte[] getBytesFromBitmap(Bitmap bm) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        return stream.toByteArray();
+    }
+
+    /**
+     * This method converts a byte array to a bitmap image.
+     *
+     * @param byteArray
+     * @return
+     */
+    public static Bitmap getBitmapFromBytes(byte[] byteArray) {
+        return BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+
+    }
+
+    public static Bitmap getBitMap() {
+        return bitmap;
+    }
+
+    public static void setBitMap(Bitmap bm) {
+        bitmap = bm;
+    }
+
+
+}
